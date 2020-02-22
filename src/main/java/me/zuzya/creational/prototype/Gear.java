7d949d45391ce23766e4baed3ee0caa334b3f309
zaripov <zaripov@tochka.com>
2020-02-22 21:53:47 +0500

    prototype + singleton

---
+++ src/main/java/me/zuzya/creational/prototype/Gear.java
@@ -0,0 +1,24 @@
+package me.zuzya.creational.prototype;
+
+import me.zuzya.creational.core.other.Power;
+
+public class Gear implements Cloneable {
+
+    private Power power;
+
+    public Gear(Gear gear) {
+
+        this.power = gear.power;
+    }
+
+    /**
+     * Prototype method
+     * 
+     * @param gear
+     * @return
+     */
+    public Gear clone(Gear gear) {
+
+        return new Gear(gear);
+    }
+}
